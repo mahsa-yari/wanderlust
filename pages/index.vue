6fa5e57e603913c0d99523d7c62eacd906af2bb8
Mahsa Allahyari <mahsa.allahyari@ice.global>
2024-03-03 14:22:50 +0330

    update fragment

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-      <HomeBanner />
-      <h2 class="text-4xl font-bold	text-center mt-10">Top things to do in United Arab Emirates</h2>
-      <HomeProductList />
-  </div>
+  <HomeBanner />
+  <h2 class="text-4xl font-bold text-center mt-10">
+    Top things to do in United Arab Emirates
+  </h2>
+  <HomeProductList />
 </template>
